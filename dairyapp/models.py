f8d342d5ef0d8565ef6f7acf890037e08d876de6
wynardtage0 <fishface6666@mail.ru>
2024-08-17 19:02:02 +0700

    + feature

--- dairyapp/models.py
+++ dairyapp/models.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from datetime import datetime
+
+from django.http import HttpRequest
 
 
 class Day(models.Model):
@@ -12,12 +15,26 @@ class Quest(models.Model):
         ordering = "completed_at", "created_at"
     created_at = models.DateTimeField(auto_now_add=True)
     completed_at = models.DateTimeField(null=True, blank=True)
+    # Hold description
+    theme_description = models.CharField(max_length=500, null=True, blank=True)
+    # Rooms
+    total_tasks = models.SmallIntegerField(default=0)
+    # Completed Rooms
+    total_completed_tasks = models.SmallIntegerField(default=0)
+    # deaths
+    errors = models.JSONField(default=list)
     complete_description = models.TextField(max_length=1000, null=True, blank=True)
     origin = models.ForeignKey("Origin", on_delete=models.SET_NULL, null=True)
+
+    def clean(self):
+        super().clean()
+        if self.total_tasks < self.total_completed_tasks:
+            raise ValidationError
 
     def save(
         self, force_insert=False, force_update=False, using=None, update_fields=None
     ):
+        self.clean()
         if not self.completed_at and self.complete_description:
             self.completed_at = datetime.now()
         return super().save(force_insert, force_update, using, update_fields
