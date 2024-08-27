007247939b14f5de8f14af2bcbdfafba15e222df
wynardtage0 <fishface6666@mail.ru>
2024-08-27 23:26:26 +0700

    tiny fix 2

--- diary/settings.py
+++ diary/settings.py
@@ -70,7 +70,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "diaryapp.apps.DairyappConfig",
+    "diaryapp.apps.DiaryappConfig",
 ]
 
 MIDDLEWARE = [
